12a643bba53b79812192b5c8e24ff02ab2e72741
Shivam Gangwar <shivamgangwar@Shivams-MacBook-Air.local>
2025-06-18 23:41:55 +0530

    fixed-assignee-issue

--- src/app/task.ts
+++ src/app/task.ts
@@ -4,10 +4,10 @@ export class Task{
     desc: string = "";
     priority: string = "Medium"; // Added priority field
     dueDate: string = ""; // Added date field
-    assignedTo: string = ""; // Added assignedTo field
+    assignee: string = ""; // Added assignedTo field
     active: boolean = false;
 
-    constructor(title: string, desc: string, active: boolean, status: string = "Pending", priority: string = "Medium", dueDate: string = "", assignedTo: string = "") {
+    constructor(title: string, desc: string, active: boolean, status: string = "Pending", priority: string = "Medium", dueDate: string = "", assignee: string = "") {
     
         this.title = title;
         this.desc = desc;
@@ -15,7 +15,7 @@ export class Task{
         this.status = status;
         this.priority = priority;
         this.dueDate = dueDate;
-        this.assignedTo = assignedTo;
+        this.assignee = assignee;
     }
 
 }
